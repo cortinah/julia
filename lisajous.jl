c04c58b7018d5634bf38f91540f5632d976e699b
Hernando Cortina <hch@alum.mit.edu>
2024-02-24 11:54:02 -0500

    9x9

--- lisajous.jl
+++ lisajous.jl
@@ -1,16 +1,16 @@
 using Plots
 
 
-a = [1,3,5,13]
-b = [1,5,7,15]
+a = [1, 3, 5, 7,  9, 11, 13, 15, 17]
+b = [1, 5, 7, 9, 11, 13, 15, 17, 19]
 p = []
 δ = π/2
 t = LinRange(-π, π, 300)
 
-for i ∈ 1:4
+for i ∈ 1:9
     x = sin.(a[i] .* t .+ δ)
     y = sin.(b[i] .* t)
     push!(p, plot(x, y, legend = false, aspect_ratio= :equal))
 end
 
-plot(p..., layout=4, size=(600,600))
+plot(p..., layout=9, size=(800,800))
