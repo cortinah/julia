c2cd55469131205b7c9a82987cae1dfa4357ce1d
Hernando Cortina <hch@alum.mit.edu>
2024-05-19 13:59:29 -0400

    heart

--- drawheart.jl
+++ drawheart.jl
@@ -3,7 +3,7 @@ using Plots
 f(x) = ((x^2)^(1/3))+((exp(1))/3)*((π-(x^2))^(1/2))*sin(a*π*x)
 a = 30
 
-plot(f, -sqrt(π) , sqrt(π))
+plot(f, -sqrt(π) , sqrt(π), legend=:false, showaxis=:false, color=:red, size=(800,800))
 
 
 # limits sqrt(pi)
