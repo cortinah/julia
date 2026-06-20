1479b5daed1dd4f8fff9daa3076d7538b7576b5c
Hernando Cortina <hch@alum.mit.edu>
2026-06-20 14:42:52 -0400

    update heart

--- drawheart.jl
+++ drawheart.jl
@@ -1,10 +1,17 @@
 using Plots
 
 f(x) = ((exp(1))/3)*((π-(x^2))^(1/2))*sin(a*π*x) + ((x^2)^(1/3))
-a = 20
+a = 15
 
 plot(f, -sqrt(π) , sqrt(π), legend=:false, showaxis=:false, grid= :false, color=:red, size=(800,800), width=4)
 
 
 # limits sqrt(pi)
 #plot(-1.771:0.001:1.771,f.(-1.771:0.001:1.771))
+
+
+f(x) = sqrt(3.1-(x^2))*sin(45*x) + (x^2)^(1/3)
+
+f(x) = (x^2)^(1/3) + sin(45*x)*sqrt(3.1-(x^2))
+
+plot(f, -sqrt(π) , sqrt(π), legend=:false, showaxis=:false, grid= :false, color=:red, size=(800,800), width=4)
